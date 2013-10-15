a1ba2f0a19825312a39529a5427777be760cb0a9
Charles Marion <charles.marion@kitware.com>
2013-10-15 11:12:11 +0200

    BUG: remove useless defined

--- constant/module.php
+++ constant/module.php
@@ -9,17 +9,4 @@ This software is distributed WITHOUT ANY WARRANTY; without even
 the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 PURPOSE.  See the above copyright notices for more information.
 =========================================================================*/
-
-define("RESOURCE_TYPE_NOT_DEFINED", 0);
-define("RESOURCE_TYPE_PUBLICATION", 1);
-define("RESOURCE_TYPE_TUTORIAL", 2);
-define("RESOURCE_TYPE_SOFTWARE", 3);
-define("RESOURCE_TYPE_PLUGIN", 4);
-define("RESOURCE_TYPE_DATASET", 5);
-
-define("RESOURCE_TYPE_ETERNAL_PUBLICATION", 100);
-define("RESOURCE_TYPE_ETERNAL_TUTORIAL", 101);
-define("RESOURCE_TYPE_ETERNAL_SOFTWARE", 102);
-define("RESOURCE_TYPE_ETERNAL_PLUGIN", 103);
-define("RESOURCE_TYPE_ETERNAL_DATASET", 104);
 ?>
