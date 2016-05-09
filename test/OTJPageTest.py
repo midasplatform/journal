d646fdd6dd56ee1710061a534026bc20d8aa9ac9
Betsy McPhail <betsy.mcphail@kitware.com>
2016-05-09 17:31:54 -0400

    Indentation changes

--- test/OTJPageTest.py
+++ test/OTJPageTest.py
@@ -32,14 +32,14 @@ class test_otj(unittest.TestCase):
     driver.close()
 
   def clickAndCheck(self,item,countClass):
-      numElements = len(driver.find_elements_by_class_name(countClass))
-      item.click()
-      time.sleep(5)
-      numElements_post = len(driver.find_elements_by_class_name(countClass))
-      print "started with %s elements, found %s elements after" % (numElements,numElements_post)
-      item.click()
-      self.assertFalse(numElements == numElements_post)
-      time.sleep(3)
+    numElements = len(driver.find_elements_by_class_name(countClass))
+    item.click()
+    time.sleep(5)
+    numElements_post = len(driver.find_elements_by_class_name(countClass))
+    print "started with %s elements, found %s elements after" % (numElements,numElements_post)
+    item.click()
+    self.assertFalse(numElements == numElements_post)
+    time.sleep(3)
 
   def test_text_search(self):
     searchbox = driver.find_element_by_id("live_search")
